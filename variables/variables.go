a060bc0afae0b6345ba97458e974e3edb37e4b36
mehedihasan722 <mjimehedi99@google.com>
2024-09-13 23:06:02 +0600

    🚨 Variables Complete
    giva

--- variables/variables.go
+++ variables/variables.go
@@ -19,6 +19,21 @@ func main() {
 	lastName = "Hossain"
 
 	fmt.Println(firstName,lastName,mod)
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b,c int = 1, 2
+	fmt.Println(b, c)
+
+	var d  = true 
+	fmt.Println(d)
+
+	var e int 
+	fmt.Println(e)
+
+	f := "Appple"
+	fmt.Println(f)
 }
 
 /*
