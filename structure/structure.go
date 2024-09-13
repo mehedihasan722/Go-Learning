3b1cba0e8225f08cd1a41d610eaf5ee019041be4
mehedihasan722 <mjimehedi99@google.com>
2024-09-13 11:56:04 +0600

    🔖 Structure complete
    ga

---
+++ structure/structure.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt" 
+
+type Student struct {
+	id int
+	address string
+	age int
+}
+
+func displayInfo(s Student) {
+	fmt.Println(s.id)
+	fmt.Println(s.address)
+	fmt.Println(s.age)
+}
+
+func main() {
+	mehedi := Student{101, "lal khan bazar", 30}
+	displayInfo(mehedi)
+}
