c302f0fa7696cf0a3780d65353660e4eebc74cc2
mehedihasan722 <mjimehedi99@google.com>
2024-09-13 23:01:39 +0600

    🔒️ Values Complete
    afa

---
+++ values/values.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("go"+"lang")
+
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Printf("7.0 / 3.0 = %.2f\n", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
